de451b70d298ed90bf0b374c8fff4086f163a1b2
kreifeur <kreifeur669@gmail.com>
2025-09-27 14:09:37 +0200

    events and blogs api

--- app/api/members/register.js
+++ app/api/members/register.js
@@ -1,5 +1,5 @@
-import { connectToDatabase } from '../../../lib/mongodb';
-import { Member } from '../../../models/Member';
+import { connectToDatabase } from '../lib/mongodb';
+import { Member } from '../models/Member';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
